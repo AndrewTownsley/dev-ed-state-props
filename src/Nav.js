a113042c8b9972c23ad0ed5d803bf92e4405402d
Andrew TOwnsley <atownsley86@gmail.com>
2021-04-25 22:29:56 -0600

    Basic file setup Practice, created a new component and rendered it to the DOM

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+    <h3>Logo</h3>
+    <ul>
+      <li>About</li>
+      <li>Home</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+    )
+}
+
+export default Nav;
